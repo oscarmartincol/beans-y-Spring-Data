fe7bd45a29a035ac74a036a08ccd60922fe02f67
Oscar Martinez <oscar_mar307@outlook.com>
2023-07-19 17:18:08 -0500

    Almacenamiento de datos.

--- ejercicio3/ejercicio3/src/main/java/com/example/ejercicio3/Ejercicio3Application.java
+++ ejercicio3/ejercicio3/src/main/java/com/example/ejercicio3/Ejercicio3Application.java
@@ -2,12 +2,26 @@ package com.example.ejercicio3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Ejercicio3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Ejercicio3Application.class, args);
+
+		ApplicationContext context = SpringApplication.run(Ejercicio3Application.class, args);
+
+		CocheRepository repository = context.getBean(CocheRepository.class);
+
+		System.out.println("find");
+
+		//Crear y almacenar coche en base de datos
+
+		Coche toyota = new Coche(null, "Toyota", "Prius", 2010);
+		repository.save(toyota);
+
+		//Recuperar todos
+		System.out.println(repository.findAll());
 	}
 
 }
